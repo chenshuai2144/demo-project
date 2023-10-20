f0116b386fe157b358adabb70e6ddb9a517f0f5b
期贤 <qixian.cs@antgroup.com>
2023-10-20 10:30:33 +0800

    初始化项目

--- simple/src/pages/index.tsx
+++ simple/src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>这里是一个超级简单的项目</h1>
     </div>
   );
 }
