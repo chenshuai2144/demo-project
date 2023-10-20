ca989d15f5a60db84cad0fcb53f437ad3438a61c
期贤 <qixian.cs@antgroup.com>
2023-10-20 11:14:57 +0800

    prettier all code

--- simple/.umirc.ts
+++ simple/.umirc.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'umi';
+import { defineConfig } from "umi";
 
 export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  routes: [{ path: '/', component: '@/pages/index' }],
-  fastRefresh: {},
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
+  ],
+  npmClient: "pnpm",
 });
